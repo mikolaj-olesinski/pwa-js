832fcce17cdbc81345b2366532c658803d66c97c
Mikołaj Olesiński <272702@student.pwr.edu.pl>
2025-05-04 23:34:22 +0200

    deleted .gitignore

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyCAOtXdPDg-bu4CXLJk1q7FEXbi6d5loc0",
+    authDomain: "koty-pwa.firebaseapp.com",
+    projectId: "koty-pwa",
+    storageBucket: "koty-pwa.firebasestorage.app",
+    messagingSenderId: "839213260571",
+    appId: "1:839213260571:web:c803fcdcd72b72b9db3dbe",
+    measurementId: "G-WSV2QP2QF9"
+};
+
+// Eksportowanie konfiguracji jako domyślny eksport
+export default firebaseConfig;
